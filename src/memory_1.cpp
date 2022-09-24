be02243c36dbfcfee182f168211accc3d7129379
MeSSM <elmessmehdi@gmail.com>
2022-09-24 05:55:00 +0100

    nullptr add

--- src/memory_1.cpp
+++ src/memory_1.cpp
@@ -10,8 +10,11 @@ using namespace std;
 int main(int argc, char** argv) {
 	Distance_wrapper d1 = Distance_wrapper(new Distance_meter(1));
 	Distance_wrapper d2 = Distance_wrapper(new Distance_leagues(3));
+	Distance_wrapper d3 = Distance_wrapper(nullptr);
 	 
 	cout << d1 << " vs "  << d2 << " => " << (d1 < d2) << " | " << (d1 == d2) << (d1 > d2) << endl;
+	
+	cout << d3 << endl;
 	return 0;
 }
 
